774c979143c4e379616c55797946a5cb81f3b114
satinder <satindersingh472@gmail.com>
2023-02-06 23:08:24 -0700

    started

---
+++ functions.py
@@ -0,0 +1,32 @@
+# define a function name calculate maths
+#define add, sub , multiply and divide functions with in calculate maths functions
+
+
+
+def calculate_maths(operation):
+    def add(n1,n2):
+        return n1 + n1
+    def subtract(n1,n2):
+        return n1 - n2
+    def divide(n1,n2):
+        return n1/n2
+    def multiply(n1,n2):
+        return n1 * n2
+
+
+    if operation == '+':
+        return add
+    elif operation == '-':
+        return subtract
+    elif operation == '/':
+        return divide
+    elif operation == '*':
+        return multiply
+
+
+
+
+
+
+
+
